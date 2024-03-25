c717788b886cda191c5327c4020693d8863ebd46
Zuzanna Z <zuzekzych@gmail.com>
2024-03-25 20:48:59 +0100

    WIP add lesson mechanics

--- lekcjeDnia.php
+++ lekcjeDnia.php
@@ -29,6 +29,7 @@ include "conn.php";
      // can we have it have an 'ghost' so it doesnt appear out of nowhere?
      if (isset($_POST['przedmiot'])) {
           echo "<p>Wybrany przedmiot - ".$_POST['przedmiot']."</p>";
+           $postPrzed = $_POST['przedmiot'];
           ?>
                <!-- action można na .php ustawić -->
           <form action="" method="post">
@@ -42,23 +43,52 @@ include "conn.php";
                </select>
 
                <label for="sala">Sala</label>
-          <input type="text" name="sala" id="sala" size="5">
+               <select name="sala" id="sala">
+               <?php
+                         foreach ($sala as $elSala) {
+                         echo "<option value='".$elSala['numer']."'>".$elSala['numer']."</option>";
+                         //TODO: - actually addingg the lesson
+                         }
+                    ?>
+               </select>
 
           <button type="submit">Zapisz lekcję</button>
           </form>
           <?php
 }
-//walidacja i unieszkodliwienie sala 
 //maybe dodanie pustej opcji typu "wybierz" w select i dodać sprawdzanie czy opcja nie jest ""
 if (isset($_POST['sala']) && trim($_POST['sala']) != "" && isset($_POST['nauczyciel']) && trim($_POST['nauczyciel']) != "") {
-     
-     $dodLekcje = "";
-if (mysqli_query($con, $dodLekcje)) {
-     # code...
-}
+     global $postPrzed;
+     // $_POST['sala'] = mysqli_real_escape_string($conn, $_POST['sala']);
+     $_POST['nauczyciel'] = mysqli_real_escape_string($conn, $_POST['nauczyciel']);
 
-     unset($_POST['sala']);
-     unset($_POST['nauczyciel']);
+     // $dodSale = "INSERT INTO `sala` (`id`, `numer`) VALUES (NULL, '".htmlentities($_POST['sala'])."')";
+     // if (mysqli_query($conn, $dodSale)) {
+          //could redo this to have the id already in select so i won't have to do this whole thing
+          $sqlSala = "";
+          $sqlNau = "";
+          $sqlPrzedmi = "SELECT id from przedmiot WHERE nazwa = '".$postPrzed."'";
+          // $rSala = mysqli_query($conn, $sqlSala);
+          // $rNau = mysqli_query($conn, $sqlNau);
+          $rPrzedmi = mysqli_query($conn, $sqlPrzedmi);
+          $idSala;
+          $idNauczyciela;
+          $idPrzedmiotu = mysqli_fetch_assoc($rPrzedmi);
+
+          echo "<h1>".$idPrzedmiotu."</h1>";
+//!!!
+          // $dodLekcje = "INSERT INTO `lekcja` (`id`, `id_sali`, `id_nauczyciela`, `id_przedmiotu`) VALUES (NULL, '$idSala', '$idNauczyciela', '$idPrzedmiotu');";
+
+          // if (mysqli_query($conn, $dodLekcje)) {
+          //      echo "<p>Dodawanie lekcji...</p>";
+          //      unset($_POST['sala']);
+          //      unset($_POST['nauczyciel']);
+          // }    
+          
+     // } else {
+     //      echo "<p>Błąd dodawanie sali!</p>";
+     // }
+     
 }
 
      ?>
