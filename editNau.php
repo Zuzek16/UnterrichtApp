8d0dca8a6ed93704c94622dafdf47524d04d45bc
Zuzek16 <zuzekzych@gmail.com>
2024-05-15 20:56:29 +0200

    form for edit teacher + start of sql

--- editNau.php
+++ editNau.php
@@ -3,7 +3,7 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Edycja klasy</title>
+     <title>Edycja danych nauczyciela</title>
      <link rel="stylesheet" href="styl.css">
 </head>
 <body>
@@ -12,31 +12,85 @@
      include_once "conn.php";
      include_once "getAll.php";
      addheader();?>
-<h2 class="pageFunc">Edytowanie nauczyciela</h2>
+<h2 class="pageFunc">Edytowanie danych nauczyciela</h2>
 <div class="dodKlase">
 <?php
      if (!isset($_GET['id'])) {
           echo "<p>Nie udało się pobrać informacji. Proszę spróbować jescze raz</p>
-          <a href='klasy.php'>Powrót</a>
+          <a href='nau.php'>Powrót</a>
           ";
      } else {
-          $nazwaKlasy = "nr. ".$_GET['id'];
-
-          global $planLekcjiKlasy;
+          $imie = "";
+          $nazwisko = "";
+          // var_dump($nauczyciele);
           foreach ($nauczyciele as $key => $value) {
                if ($value['id'] == $_GET['id']) {
-                    $nazwaKlasy = $value['nazwa'];
+                    $imie = $value['imie'];
+                    $nazwisko = $value['nazwisko'];
                }
           }
-          echo "<form method='POST' action='#'>
-          <label for='nowyPlan'>Wybierz nowy plan lekcji dla klasy ".$nazwaKlasy."</label>
-          <select name='nowyPlan' id='nowyPlan'>";
-          
-          foreach ($planyLekcji as $key => $value) {
-               echo "<option value='".$value['id']."'>".$value['id']."</option>";
+
+          echo "<form method='POST' action='#'>";
+          echo "<label for='imie'>Imię: </label>";
+          echo "<input type='text' value='$imie'>";
+
+          echo "<label for='nazwisko'>Nazwisko: </label>";
+          echo "<input type='text' value='$nazwisko'>";
+          echo "<p>Nauczane przedmioty</p>";
+          echo '<div class="checkboxes">';
+          $nauPrzed = [];
+          foreach ($nauczany_przedmiot as $key => $value) {//spr jakie przedmioty uczą / move to a fn
+               foreach ($value as $key2 => $value2) {
+                    if ($imie." ".$nazwisko == $value2[1] && $_GET['id'] == $value2[0]) {
+                         array_push($nauPrzed, $key);
+                    //    echo "<p>$key</p>";
+                   }
+               }
           }
-          
-          echo "</select><button type='submit'>Zapisz</button></form>";
+
+          global $przedmiot;
+          foreach ($przedmiot as $el) {
+               $printed = false;
+               foreach ($nauPrzed as $key => $value) {
+                    if ($el['nazwa'] == $value) {
+                         echo "<input type='checkbox' name='".$el['id']."' id='".$el['id']."' value='".$el['id']."' checked>";
+              echo "<label for='".$el['id']."'>".$el['nazwa']."</label><br>";
+              $printed = true;
+                    }
+               }
+               if ($printed == false) {
+                    echo "<input type='checkbox' name='".$el['id']."' id='".$el['id']."' value='".$el['id']."'>";
+              echo "<label for='".$el['id']."'>".$el['nazwa']."</label><br>";
+               }
+              
+          }
+
+
+
+          echo "</div>";
+          //  echo "<input type='checkbox' checked>";
+
+          echo "<label for='szkola'>Szkoła w której uczy:</label>";
+          echo "<select name='szkola' id='szkola'>";
+
+          $aktSzkola = "";
+          foreach ($nauSzkoly as $row => $value) {
+               if ($_GET['id'] == $value['id_nauczyciela']) {
+                   if (trim($value['nazwa']) != "") {
+                    $aktSzkola = $value['nazwa'];
+                   }
+               }
+          }
+
+          foreach ($klasaSzkoly as $key => $value) {
+               if ($key == $aktSzkola) {
+                    echo "<option value='".$key."' selected >".$key."</option>";          
+               } else {
+                    echo "<option value='".$key."' >".$key."</option>";          
+               }
+          }
+          echo "</select>";
+          echo "<button type='submit'>Zapisz</button></form>";
      }
      ?>
      <?php
